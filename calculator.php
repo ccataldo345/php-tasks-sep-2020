c847015a940424aade81b46924487ed261771c5f
Chris C <epost345@gmail.com>
2020-09-23 14:13:17 +0300

    Add private and const

--- calculator.php
+++ calculator.php
@@ -1,32 +1,38 @@
 <?php
 
-class calculator {
+class calculator
+{
 
-  public $value;
-  public $taxRate;
-  public $installments;
-  // private const BASE_INDEX = 11%;
+  private $value;
+  private $taxRate;
+  private $installments;
+  private const BASE_INDEX = 0.11;
 
-  public function __construct($value, $taxRate, $installments) {
+  public function __construct($value, $taxRate, $installments)
+  {
     $this->value = $value;
     $this->tax = $taxRate;
     $this->installments = $installments;
   }
 
-  public function calculateBasePrice($value) {
-    return number_format($value * 0.11, 2,".","");
+  public function calculateBasePrice($value)
+  {
+    return number_format($value * self::BASE_INDEX, 2, ".", "");
   }
 
-  public function calculateCommission($value) {
-    return number_format($value * 0.11 * 0.17, 2,".","");
+  public function calculateCommission($value)
+  {
+    return number_format($value * self::BASE_INDEX * 0.17, 2, ".", "");
   }
 
-  public function calculateTax($value, $taxRate) {
-    return number_format($value * 0.11 * $taxRate/100, 2,".","");
-  }
-   
-  public function calculateTotalCost($value, $taxRate) {
-    return number_format((($value * 0.11) + ($value * 0.11 * 0.17) + ($value * 0.11 * $taxRate/100)), 2,".","");
+  public function calculateTax($value, $taxRate)
+  {
+    return number_format($value * self::BASE_INDEX * $taxRate / 100, 2, ".", "");
   }
 
+  public function calculateTotalCost($value, $taxRate)
+  {
+    return number_format((($value * self::BASE_INDEX) + ($value * self::BASE_INDEX * 0.17) +
+     ($value * self::BASE_INDEX * $taxRate / 100)), 2, ".", "");
+  }
 }
